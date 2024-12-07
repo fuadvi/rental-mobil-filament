038d683e4df0a711b3e89a2daeae5474db67c99f
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2024-12-07 19:24:09 +0700

    up size upload image

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
-// use Illuminate\Support\Facades\Route;
+ use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+Route::get('phpinfo', function () {
+  phpinfo();
+});
